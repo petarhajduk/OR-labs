d58c56819bc74b1ebf1bb1b911efd6da96ad5132
petarhajduk <hajduk.petar@gmail.com>
2023-11-12 17:59:48 +0100

    extended database to 10 rows

--- dbInit.sql
+++ dbInit.sql
@@ -18,20 +18,37 @@ closed boolean,
 foreign key (za_koga_id) references manager(manager_id) on delete cascade);
 
 insert into manager (username, email, password_hash) values
-('menadžer1', 'menadžer1@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f');
+('menadžer1', 'menadžer1@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer2', 'menadžer2@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer3', 'menadžer3@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer4', 'menadžer4@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer5', 'menadžer5@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer6', 'menadžer6@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer7', 'menadžer7@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer8', 'menadžer8@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer9', 'menadžer9@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f')
+('menadžer10', 'menadžer10@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f');
 
-insert into manager (username, email, password_hash) values
-('menadžer2', 'menadžer2@gmail.com', 'jdn934hf937bo3ubf934fvb93b4f');
 
 insert into post (thread_id, za_koga_id, subjectt, poruka, 
 manager_vidio, from_manager, time_created, time_manager_saw, closed) values
 (1, 1, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:00:00', null, 0),
 (1, 1, 'tema1', 'poruka2', 1, 1, '2023-11-01 12:05:00', '2023-11-01 12:06:00', 0),
-(1, 1, 'tema1', 'poruka3', 0, 0, '2023-11-01 12:10:00', null, 0),
-(1, 1, 'tema1', 'poruka4', 1, 1, '2023-11-01 12:15:00', '2023-11-01 12:15:00', 0),
-(1, 1, 'tema1', 'poruka5', 0, 0, '2023-11-01 12:20:00', null, 0),
-(2, 2, 'tema2', 'poruka6', 1, 0, '2023-11-01 13:00:00', null, 1),
-(2, 2, 'tema2', 'poruka7', 1, 1, '2023-11-01 13:05:00', '2023-11-01 13:05:00', 1),
-(2, 2, 'tema2', 'poruka8', 1, 0, '2023-11-01 13:10:00', null, 1),
-(2, 2, 'tema2', 'poruka9', 1, 1, '2023-11-01 13:15:00', '2023-11-01 13:15:00', 1),
-(2, 2, 'tema2', 'poruka10', 1, 0, '2023-11-01 13:20:00', null, 1);
+(2, 2, 'tema2', 'poruka1', 1, 0, '2023-11-01 13:00:00', null, 1),
+(2, 2, 'tema2', 'poruka2', 1, 1, '2023-11-01 13:05:00', '2023-11-01 13:05:00', 1),
+(3, 3, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:10:00', null, 0),
+(3, 3, 'tema1', 'poruka2', 1, 1, '2023-11-01 12:15:00', '2023-11-01 12:15:00', 0),
+(4, 4, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:20:00', null, 0),
+(4, 4, 'tema2', 'poruka2', 1, 1, '2023-11-01 13:15:00', '2023-11-01 13:15:00', 1),
+(5, 5, 'tema2', 'poruka1', 1, 1, '2023-11-01 13:15:00', null, 1),
+(5, 5, 'tema2', 'poruka2', 1, 0, '2023-11-01 13:20:00', '2023-11-01 13:15:00', 1)
+(6, 6, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:00:00', null, 0),
+(6, 6, 'tema1', 'poruka2', 1, 1, '2023-11-01 12:05:00', '2023-11-01 12:06:00', 0),
+(7, 7, 'tema2', 'poruka1', 1, 0, '2023-11-01 13:00:00', null, 1),
+(7, 7, 'tema2', 'poruka2', 1, 1, '2023-11-01 13:05:00', '2023-11-01 13:05:00', 1),
+(8, 8, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:10:00', null, 0),
+(8, 8, 'tema1', 'poruka2', 1, 1, '2023-11-01 12:15:00', '2023-11-01 12:15:00', 0),
+(9, 9, 'tema1', 'poruka1', 0, 0, '2023-11-01 12:20:00', null, 0),
+(9, 9, 'tema2', 'poruka2', 1, 1, '2023-11-01 13:15:00', '2023-11-01 13:15:00', 1),
+(10, 10, 'tema2', 'poruka1', 1, 1, '2023-11-01 13:15:00', null, 1),
+(10, 10, 'tema2', 'poruka2', 1, 0, '2023-11-01 13:20:00', '2023-11-01 13:15:00', 1);
